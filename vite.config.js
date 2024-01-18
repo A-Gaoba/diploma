284bf9c9d0df9c7a640f79a0c857dd97c36a1486
a-goaba <abdulrahman1997abdo@gmail.com>
2024-01-18 05:53:38 +0500

    adding the addStudnet component

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['/vercel/path0/client/src/main.jsx']
-      // Add any other external modules if needed
-    },
-  },
 });
