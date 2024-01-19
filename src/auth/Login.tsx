1b7a4a95985a7007951cf6aedeadf9bd7e1c1368
a-goaba <abdulrahman1997abdo@gmail.com>
2024-01-19 08:23:39 +0500

    add the login component

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -37,49 +37,49 @@ const Login = ({ onLogin }) => {
         className="h-screen flex flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0 bg-dark-purple"
         onKeyPress={handleKeyPress}
       >
-        <div className="md:w-1/3 max-w-sm">
-          <img
-            src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-            alt="Sample image"
-          />
-        </div>
-        <div className="md:w-1/3 max-w-sm">
-          <input
-            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <div className="mt-4 flex justify-between font-semibold text-sm">
-            <label className="flex text-slate-100 hover:text-blue-300 -600 cursor-pointer">
-              <input className="mr-1" type="checkbox" />
-              <span>Remember Me</span>
-            </label>
-            <a
-              className="text-white hover:text-blue-300 hover:underline hover:underline-offset-4"
-              href="#"
-            >
-              Forgot Password?
-            </a>
+          <div className="md:w-1/3 max-w-sm">
+            <img
+              src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              alt="Sample image"
+            />
           </div>
-          <div className="text-center md:text-left">
-            <button
-              className="mt-4 bg-white hover:bg-blue-300 px-4 py-2 text-black font-bold uppercase rounded text-xs tracking-wider"
-              type="button"
-              onClick={handleLogin}
-            >
-              Login
-            </button>
+          <div className="md:w-1/3 max-w-sm bgda p-12 h-96 flex flex-col justify-center">
+            <input
+              className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded mt-4"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="mt-4 flex justify-between font-semibold text-sm">
+              <label className="flex text-slate-100 hover:text-blue-300 -600 cursor-pointer">
+                <input className="mr-1" type="checkbox" />
+                <span>Remember Me</span>
+              </label>
+              <a
+                className="text-white hover:text-blue-300 hover:underline hover:underline-offset-4"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+            <div className="text-center md:text-left">
+              <button
+                className="mt-4 bg-white hover:bg-blue-300 px-4 py-2 text-black font-bold uppercase rounded text-xs tracking-wider"
+                type="button"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </div>
           </div>
-        </div>
       </section>
       <ToastContainer />
     </>
