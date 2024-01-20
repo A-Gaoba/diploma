7326fc07bae75cd1d1bdb47f213382f2db4acedc
a-goaba <abdulrahman1997abdo@gmail.com>
2024-01-20 06:30:27 +0500

    add subjects component

--- src/pages/Subjects/Home.tsx
+++ src/pages/Subjects/Home.tsx
@@ -22,7 +22,7 @@ const Home: React.FC<HomeProps> = () => {
         {classes.map((cls) => (
           <button
             key={cls}
-            className={`px-4 py-2 rounded ${cls === selectedClass ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'
+            className={`px-4 py-2 rounded ${cls === selectedClass ? 'bg-white text-dark-purple' : 'bg-dark-purple text-white'
               }`}
             onClick={() => handleClassChange(cls)}
           >
@@ -32,7 +32,7 @@ const Home: React.FC<HomeProps> = () => {
       </div>
 
       {/* Subject Boxes */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 2xl:grid-cols-6 gap-8 mt-8">
         {filteredSubjects.map((subject: Subject, index: number) => (
           <SubjectBox key={index} subject={subject} />
         ))}
@@ -47,7 +47,7 @@ interface SubjectBoxProps {
 
 const SubjectBox: React.FC<SubjectBoxProps> = ({ subject }) => (
   <div className="bg-white p-4 rounded shadow-md">
-    <img src={subject.image} alt={`${subject.name} Image`} className="w-full h-32 object-cover mb-4 rounded" />
+    <img src={subject.image} alt={`${subject.name} Image`} className="w-full h-48 object-cover mb-4 rounded bg-dark-purple" />
     <h3 className="text-xl font-semibold mb-2">{subject.name}</h3>
     <p className="text-gray-600">Teacher: {subject.teacher}</p>
   </div>
